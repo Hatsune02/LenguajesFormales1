090407ceea488d29093261022d6ec67e421ce477
Dónavin Tzunún <tzunundonavin@gmail.com>
2023-07-29 23:26:42 -0600

    Testing the analizer, seems fine

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import backend.Analizer;
+
 public class Main {
     public static void main(String[] args) {
-
+        String xd = "gatos perros chuchos pepo 8956 9.96 * = == *= >=";
+        Analizer analizer = new Analizer();
+        analizer.analize(xd);
 
     }
 }
